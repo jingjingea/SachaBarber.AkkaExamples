60f41293c70871cf0bd6fe2007a8cd8ca9f73873
kimhana <jingjingea@unagel.com>
2018-01-05 17:01:58 +0900

    altery, netty 차이 방식 추가

--- Clustering/src/main/scala-2.11/ClientJobTransformationSendingActor.scala
+++ Clustering/src/main/scala-2.11/ClientJobTransformationSendingActor.scala
@@ -1,9 +1,9 @@
-import akka.actor.Actor
-import akka.actor.ActorPath
-import akka.cluster.client.{ClusterClientSettings, ClusterClient}
+import akka.actor.{Actor, ActorPath}
+import akka.cluster.client.{ClusterClient, ClusterClientSettings}
 import akka.pattern.Patterns
-import sample.cluster.transformation.{TransformationResult, TransformationJob}
+import sample.cluster.transformation.{TransformationJob, TransformationResult}
 import akka.util.Timeout
+
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
@@ -11,8 +11,23 @@ import scala.util.{Failure, Success}
 
 class ClientJobTransformationSendingActor extends Actor {
 
+
+  // akka의 remote 방식이 tcp일 경우 akka.tcp://를 사용 하며 altery 방식일 경우 akka:// 방식을 사용
+
   val initialContacts = Set(
     ActorPath.fromString("akka.tcp://ClusterSystem@127.0.0.1:2551/system/receptionist"))
+
+
+//  ActorPath.fromString("akka.tcp://thingplug@127.0.0.1:5150/system/receptionist")
+
+//  val initialContacts = Set(
+//    ActorPath.fromString("akka://thingplug@192.168.1.74:5150/system/receptionist"),
+//    ActorPath.fromString("akka://thingplug@192.168.1.73:5150/system/receptionist"),
+//    ActorPath.fromString("akka://thingplug@192.168.1.72:5150/system/receptionist")
+//  )
+
+
+
   val settings = ClusterClientSettings(context.system)
     .withInitialContacts(initialContacts)
 
